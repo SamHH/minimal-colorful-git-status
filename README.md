097f87ed2c64f483ba7cb94151d184d844217232
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2016-12-15 18:57:01 +0000

    Add custom settings

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ An extension to subnixr's minimal zsh theme.
 
 ## Installation
 
-Source/load the extension right before `subnixr/minimal`. If you've defined any custom variables, be sure to set those _before_ sourcing this plugin/theme.
+Source/load the extension right before `subnixr/minimal`.
 
 An example with [zplug](https://github.com/zplug/zplug):
 
@@ -12,3 +12,15 @@ An example with [zplug](https://github.com/zplug/zplug):
     zplug "samhh/minimal-colorful-git-status"
     zplug "subnixr/minimal"
     zplug load
+
+## Configuration
+
+Define all variables _before_ sourcing this plugin/theme.
+
+Colors are mapped to integers identically to the table displayed on the Arch wiki [here](https://wiki.archlinux.org/index.php/zsh#Colors).
+
+- `MINIMAL_GIT_DIRTY_COLOR`: default `1` (red)
+- `MINIMAL_GIT_DIVERGED_COLOR`: default `5` (magenta)
+- `MINIMAL_GIT_BEHIND_COLOR`: default `4` (blue)
+- `MINIMAL_GIT_AHEAD_COLOR`: default `7` (white)
+- `MINIMAL_GIT_CLEAN_COLOR`: default `2` (green)
--- minimal-colorful-git-status.zsh
+++ minimal-colorful-git-status.zsh
@@ -1,3 +1,10 @@
+# Vars with default fallback settings
+MINIMAL_GIT_DIRTY_COLOR="${MINIMAL_GIT_DIRTY_COLOR:-1}" # red
+MINIMAL_GIT_DIVERGED_COLOR="${MINIMAL_GIT_DIVERGED_COLOR:-5}" # magenta
+MINIMAL_GIT_BEHIND_COLOR="${MINIMAL_GIT_BEHIND_COLOR:-4}" # blue
+MINIMAL_GIT_AHEAD_COLOR="${MINIMAL_GIT_AHEAD_COLOR:-7}" # white
+MINIMAL_GIT_CLEAN_COLOR="${MINIMAL_GIT_CLEAN_COLOR:-2}" # green
+
 minimal_vcs() {
   local branchName
   branchName="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
@@ -7,21 +14,26 @@ minimal_vcs() {
     localRemoteState="$(git status --porcelain -b)"
 
     local color
+
+    # dirty
     if echo "$localRemoteState" | grep -v '^##' &> /dev/null; then
-      # dirty
-      color="1" # red
+      color=$MINIMAL_GIT_DIRTY_COLOR
+
+    # diverged
     elif echo "$localRemoteState" | grep '^## .*diverged' &> /dev/null; then
-      # diverged
-      color="5" # magenta
+      color=$MINIMAL_GIT_DIVERGED_COLOR
+
+    # behind
     elif echo "$localRemoteState" | grep '^## .*behind' &> /dev/null; then
-      # behind
-      color="4" # blue
+      color=$MINIMAL_GIT_BEHIND_COLOR
+
+    # ahead
     elif echo "$localRemoteState" | grep '^## .*ahead' &> /dev/null; then
-      # ahead
-      color="7" # white
+      color=$MINIMAL_GIT_AHEAD_COLOR
+
+    # clean
     else
-      # clean
-      color=$MINIMAL_OK_COLOR # green by default
+      color=$MINIMAL_GIT_CLEAN_COLOR
     fi
 
     local formattedColor="%{\e[0;3${color}m%}"
